563ad2afeadc9b3fa5ac48cb566cef642939301b
suansuan <3241724463@qq.com>
2021-07-10 19:25:10 +0800

    工程搭建

---
+++ src/main/java/com/cy/psi/PsiApplication.java
@@ -0,0 +1,13 @@
+package com.cy.psi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsiApplication.class, args);
+    }
+
+}
