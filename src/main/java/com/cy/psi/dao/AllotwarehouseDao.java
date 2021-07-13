5548b6882ff1d7839e64551207cb9baf227917a4
tsy2648156617 <85813840+tsy2648156617@users.noreply.github.com>
2021-07-13 17:41:08 +0800

    三生三世

--- src/main/java/com/cy/psi/dao/AllotwarehouseDao.java
+++ src/main/java/com/cy/psi/dao/AllotwarehouseDao.java
@@ -1,6 +1,6 @@
 package com.cy.psi.dao;
 
-import com.cy.psi.entity.Allotwarehouse;
+import com.cy.psi.entity.*;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -15,6 +15,12 @@ import java.util.List;
 @Mapper
 public interface AllotwarehouseDao {
 
+    //查询仓库
+    List<BaseDepot> selectCangku();
+
+    //查询产品
+    List<BaseProduct> selectProduct();
+
     /**
      查询全部
      */
@@ -24,6 +30,17 @@ public interface AllotwarehouseDao {
      搞鸡查询
      */
     List<Allotwarehouse> selectBylikeAllotwarehouse(@Param("serialNumber") String serialNumber, @Param("documentmaker") String documentmaker, @Param("depotName") String depotName);
+
+    //新增调拨单
+    Integer insertAllotwarehouse(Allotwarehouse allotwarehouse);
+
+    //调拨新增出库单
+    Integer insertSaleDelivery(SaleDelivery saleDelivery);
+
+    //调拨新增入库单
+    Integer insertGoods(Goods goods);
+    //调拨单审核
+    Integer AllotwarehouseAudit(@Param("approval") Integer approval,@Param("channelid")Integer channelid);
 
     //逻辑删除
     Integer DelAllotwarehouse(@Param("timeLiness") Integer timeLiness, @Param("channelid")Integer channelid);
